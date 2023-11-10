4b2bab93295f4882b3945eb42fb8fb9a1395096e
Irving Poot <eduardopoot17@gmail.com>
2023-11-09 23:28:49 -0600

    Vista Home Page lista

--- views/public/index.php
+++ views/public/index.php
@@ -1,9 +1,9 @@
 <?php
     include __DIR__ ."/template/header.php";
 ?>
-<main>
-    <h1>Home</h1>
-</main>
+<?php
+    include "main.php";
+?>
 <?php
     include __DIR__ ."/template/footer.php";
 ?>
