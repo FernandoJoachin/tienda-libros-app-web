0960913893bc8d83e8c2af0106a4723bd0c0f06e
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-11-27 11:20:35 -0600

    base de datos

--- booke.sql
+++ booke.sql
@@ -16,6 +16,37 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `books` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `bookIsbn` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bookTitle` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bookAuthor` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bookEdition` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bookCategory` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bookStock` int DEFAULT NULL,
+  `imagen` varchar(45) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `bookPrice` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (7,'978-0061122415','Cien años de soledad','Gabriel García Márquez','1ra Edición','Ficción latinoamericana',23,'BookDefault.png',299.00),(8,'978-0553382563','The Da Vinci Code','Dan Brown','3ra Edición','Thriller',28,'BookDefault.png',299.00),(9,'978-0743273565','To Kill a Mockingbird	','Harper Lee','2da Edición','Novela clásica',22,'BookDefault.png',299.00);
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -52,4 +83,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-11-11 19:57:35
+-- Dump completed on 2023-11-27 11:19:50
