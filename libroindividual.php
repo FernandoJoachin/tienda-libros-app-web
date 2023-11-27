93b79c983e3f79e197c7e7014fc13570bc1cde27
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-11-26 20:05:59 -0600

    correcciones

--- libroindividual.php
+++ libroindividual.php
@@ -1,78 +1,72 @@
 <?php
+    $page = 'libroIndividual';
     include __DIR__ ."/template/header.php";
 ?>
-<div class="contenedor-titulo">
-    <picture>
-        <source srcset="./build/img/hero.avif" type="image/avif">
-        <source srcset="./build/img/hero.webp" type="image/webp">
-        <img loading="lazy" width="200" height="200" class="contenedor-titulo_fondonav" src="../../build/img/hero.png" alt="titulo">
-    </picture>
-</div>
+<?php
+    $heroTitle = 'Datos Libro';
+    include __DIR__ ."/template/hero.php";
+?>
 <main>
-    <div class="contenedor-info">
-        <div class="contenedor-izquierda">
-            <div class="contenedor-portada">
-                <picture>
-                    <source srcset="./build/img/portadalibro.avif" type="image/avif">
-                    <source srcset="./build/img/portadalibro.webp" type="image/webp">
-                    <img loading="lazy" width="200" height="200" class="contenedor-portada_img" src="../../build/img/portadalibro.png" alt="portada">
-                </picture>
-            </div>
-            <div class="pie-de-foto">
-                <span><p>“Lo m&aacute;s curioso que hay en la vida es el espect&aacute;culo de la muerte.”</p></span>
-                <div class="pie-de-foto_envio">
-                    <i class="fa-solid fa-truck fa-flip-horizontal"></i>
-                    <span><p>ENV&Iacute;O GRATIS</p></span>
+    <form class="formulario-carrito-compras" method="POST">
+        <div class="contenedor-info">
+            <div class="contenedor-izquierda">
+                <div class="contenedor-portada">
+                    <picture>
+                        <source srcset="./build/img/portadalibro.avif" type="image/avif">
+                        <source srcset="./build/img/portadalibro.webp" type="image/webp">
+                        <img loading="lazy" width="200" height="200" class="contenedor-portada_img" src="../../build/img/portadalibro.png" alt="portada">
+                    </picture>
+                </div>
+                <div class="pie-de-foto">
+                    <span><p>“Lo m&aacute;s curioso que hay en la vida es el espect&aacute;culo de la muerte.”</p></span>
+                    <div class="pie-de-foto_envio">
+                        <i class="fa-solid fa-truck fa-flip-horizontal"></i>
+                        <span><p>ENV&Iacute;O GRATIS</p></span>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="detalles-sup">
-            <span class="detalles-sup_negrita"><p>El conde de Montecristo</p></span>
-            <label class="detalles-sup_negrita">Precio MXN</label><br><br>
-            <div class="contenedor-cantidad">
-                <label>Cantidad</label>
-                <div class="btn-incrementar-decrementar">
-                    <i class="fa-solid fa-minus btncolor" id="decrement"></i>
-                    <input type="number" placeholder="1" value="1" min="1" class="input-cantidad">
-                    <i class="fa-solid fa-plus btncolor" id="increment"></i>
+            <div class="detalles-sup">
+                <span class="detalles-sup_negrita"><p>El conde de Montecristo</p></span>
+                <label class="detalles-sup_negrita">Precio MXN</label><br><br>
+                <div class="contenedor-cantidad">
+                    <label>Cantidad</label>
+                    <div class="btn-incrementar-decrementar">
+                        <i class="fa-solid fa-minus btncolor" id="decrement"></i>
+                        <input type="number" placeholder="1" value="1" min="1" class="input-cantidad">
+                        <i class="fa-solid fa-plus btncolor" id="increment"></i>
+                    </div>
+                </div>
+                <div class="contenedor-envios">
+                    <label>
+                        <input type="radio" name="envio" value="rapido">
+                        Env&iacute;o r&aacute;pido (+50 MXN)
+                    </label>
+                    <label>
+                        <input type="radio" name="envio" value="gratis">
+                        Env&iacute;o gratis
+                    </label>
+                </div>
+                <div class="contenedor-carrito">
+                    <button class="btn-add-cart">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                        Añadir al carrito
+                    </button>
                 </div>
             </div>
-            <div class="contenedor-envios">
-                <label>
-                    <input type="radio" name="envio" value="rapido">
-                    Env&iacute;o r&aacute;pido (+50 MXN)
-                </label>
-                <label>
-                    <input type="radio" name="envio" value="gratis">
-                    Env&iacute;o gratis
-                </label>
-            </div>
-            <div class="contenedor-carrito">
-                <button class="btn-add-cart">
-                    <i class="fa-solid fa-cart-shopping"></i>
-                    Añadir al carrito
-                </button>
+            <div class="detalles">
+                <span class="detalles__titulo"><p>Detalles del Libro</p></span>
+                <span class="detalles__info">
+                    <p>ISBN:</p>
+                    <p>Autor:</p>
+                    <p>Disponibles:</p>
+                    <p>Edici&oacute;n:</p>
+                    <p>Categoría:</p>
+                </span>
             </div>
         </div>
-        <div class="detalles">
-            <span class="detalles__titulo"><p>Detalles del Libro</p></span>
-            <span class="detalles__info">
-                <p>ISBN:</p>
-                <p>Autor:</p>
-                <p>Disponibles:</p>
-                <p>Edici&oacute;n:</p>
-                <p>Categoría:</p>
-            </span>
-        </div>
-    </div>
 
-    <div class="contenedor-boton">
-        <picture>
-            <source srcset="./build/img/BotonEnviar.avif" type="image/avif">
-            <source srcset="./build/img/BotonEnviar.webp" type="image/webp">
-            <img loading="lazy" class="boton_enviar" src="../../build/img/BotonEnviar.png" alt="Logo">
-        </picture>
-    </div>
+        <input type="submit" class="formulario-carrito-compras__submit" value="Enviar">
+    </form>
 </main>
 <?php
     include __DIR__ ."/template/footer.php";
--- src/js/libroindividual.js
+++ src/js/libroindividual.js
@@ -2,19 +2,14 @@ const inputCantidad = document.querySelector('.input-cantidad')
 const btnIncrement = document.querySelector('#increment')
 const btnDecrement = document.querySelector('#decrement')
 
-let valueByDefault = parseInt(inputCantidad.value)
-
 //Funciones de incremento/decremento
 btnIncrement.addEventListener('click', () => {
-    valueByDefault += 1
-    inputCantidad.value = valueByDefault
+    inputCantidad.value = parseInt(inputCantidad.value) + 1
 })
 
 btnDecrement.addEventListener('click', () => {
-    if (valueByDefault !=1) {
-        valueByDefault -= 1
-    }else{
-        inputCantidad.value = valueByDefault
+    if (inputCantidad.value !=1) {
+        inputCantidad.value = parseInt(inputCantidad.value) - 1
     }
 })
 
