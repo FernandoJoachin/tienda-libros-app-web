8618de7bcb6b6b4fb8a2d9e786801c72d0b0b23c
FernandoJoachin <97647369+FernandoJoachin@users.noreply.github.com>
2023-11-30 23:06:44 -0600

    quitar los required

--- editar.php
+++ editar.php
@@ -20,46 +20,46 @@ include __DIR__ . "/template/header-admin.php";
 
                 <form class="formulario-crear" method="POST" action="./controllers/BookController/editarLibro.php" enctype="multipart/form-data">
 
-                    <input name="id" type="hidden" value="<?= $libro->id?>" required>
+                    <input name="id" type="hidden" value="<?= $libro->id?>">
 
                     <div class="formulario-crear__campo">
-                        <input class="formulario-crear__input" name="bookIsbn" type="text" value="<?= $libro->bookIsbn?>" required>
+                        <input class="formulario-crear__input" name="bookIsbn" type="text" value="<?= $libro->bookIsbn?>">
                         <span></span>
                         <label class="formulario-crear__label">ISBN</label>
                     </div>
 
                     <div class="formulario-crear__campo">
-                        <input class="formulario-crear__input" name="bookTitle" type="text" value="<?= $libro->bookTitle?>" required>
+                        <input class="formulario-crear__input" name="bookTitle" type="text" value="<?= $libro->bookTitle?>">
                         <span></span>
                         <label class="formulario-crear__label">Título</label>
                     </div>
 
                     <div class="formulario-crear__campo">
-                        <input class="formulario-crear__input" name="bookAuthor" type="text" value="<?= $libro->bookAuthor?>" required>
+                        <input class="formulario-crear__input" name="bookAuthor" type="text" value="<?= $libro->bookAuthor?>">
                         <span></span>
                         <label class="formulario-crear__label">Autor</label>
                     </div>
 
                     <div class="formulario-crear__campo">
-                        <input class="formulario-crear__input" name="bookEdition" type="text" value="<?= $libro->bookEdition?>" required>
+                        <input class="formulario-crear__input" name="bookEdition" type="text" value="<?= $libro->bookEdition?>">
                         <span></span>
                         <label class="formulario-crear__label">Edición</label>
                     </div>
 
                     <div class="formulario-crear__campo">
-                        <input class="formulario-crear__input" name="bookCategory" type="text" value="<?= $libro->bookCategory?>" required>
+                        <input class="formulario-crear__input" name="bookCategory" type="text" value="<?= $libro->bookCategory?>">
                         <span></span>
                         <label class="formulario-crear__label">Categoría</label>
                     </div>
 
                     <div class="formulario-crear__campo">
-                        <input class="formulario-crear__input" name="bookPrice" type="number" value="<?= $libro->bookPrice?>" required>
+                        <input class="formulario-crear__input" name="bookPrice" type="number" value="<?= $libro->bookPrice?>">
                         <span></span>
                         <label class="formulario-crear__label">Precio</label>
                     </div>
 
                     <div class="formulario-crear__campo">
-                        <input class="formulario-crear__input" name="bookStock" type="number" value="<?= $libro->bookStock?>" required>
+                        <input class="formulario-crear__input" name="bookStock" type="number" value="<?= $libro->bookStock?>">
                         <span></span>
                         <label class="formulario-crear__label">Stock</label>
                     </div>
